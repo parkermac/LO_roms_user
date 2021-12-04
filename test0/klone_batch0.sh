74b39e60a8188759f600a2da52ff6ba024e07f36
Parker MacCready <p.maccready@gmail.com>
2021-12-04 11:02:57 -0800

    test0
    attempt to run the upwelling example on klone

---
+++ test0/klone_batch0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Job Name
+#SBATCH --job-name=LiveOcean
+
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Tasks per node (Slurm assumes you want to run 28 tasks per node unless explicitly told otherwise)
+#SBATCH --ntasks-per-node=4
+
+## Walltime 
+#SBATCH --time=05:00:00
+
+## Memory per node
+#SBATCH --mem=100G
+
+module purge
+module load intel/oneAPI
+NFDIR=/gscratch/macc/local/netcdf-ifort/
+export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
+
+mpirun /mmfs1/gscratch/macc/parker/LO_roms_user/test0/romsM /mmfs1/gscratch/macc/parker/LO_roms_user/test0/roms_upwelling.in > /mmfs1/gscratch/macc/parker/LO_roms_user/test0/log.txt
+
