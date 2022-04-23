a4560ef70aedcc48ad198ec73185d28e136f5689
Parker MacCready <p.maccready@gmail.com>
2022-04-23 16:09:52 -0700

    Update klone_batch0.sh

--- test0/klone_batch0.sh
+++ test0/klone_batch0.sh
@@ -20,7 +20,6 @@ module load intel/oneAPI
 NFDIR=/gscratch/macc/local/netcdf-ifort/
 export LD_LIBRARY_PATH=${NFDIR}/lib:${LD_LIBRARY_PATH}
 
-## mpirun -np 4 /mmfs1/gscratch/macc/parker/LO_roms_user/test0/romsM /mmfs1/gscratch/macc/parker/LO_roms_user/test0/roms_upwelling.in > /mmfs1/gscratch/macc/parker/LO_roms_user/test0/roms_log.txt
 RUN_DIR=/mmfs1/gscratch/macc/parker/LO_roms_user/test0
 mpirun -np 4 $RUN_DIR/romsM $RUN_DIR/roms_upwelling.in > $RUN_DIR/roms_log.txt
 
