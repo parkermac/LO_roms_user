52a6c4b81c7478143173177151f9b61bb6eb2a3f
Parker MacCready <p.maccready@gmail.com>
2024-01-21 15:22:23 -0800

    update files in upwelling example

--- upwelling/upwelling.h
+++ upwelling/upwelling.h
@@ -1,9 +1,10 @@
 /*
-** svn $Id: upwelling.h 1054 2021-03-06 19:47:12Z arango $
+** git $Id$
+** svn $Id: upwelling.h 1151 2023-02-09 03:08:53Z arango $
 *******************************************************************************
-** Copyright (c) 2002-2021 The ROMS/TOMS Group                               **
+** Copyright (c) 2002-2023 The ROMS/TOMS Group                               **
 **   Licensed under a MIT/X style license                                    **
-**   See License_ROMS.txt                                                    **
+**   See License_ROMS.md                                                     **
 *******************************************************************************
 **
 ** Options for Upwelling Test.
